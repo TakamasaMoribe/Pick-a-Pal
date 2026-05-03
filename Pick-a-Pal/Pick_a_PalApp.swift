66a3745d12b1c2ba2ff2e00898340c0fc6189a4c
森部高昌 <takamasamoribe@gmail.com>
2026-05-03 10:12:08 +0900

    Initial Commit

---
+++ Pick-a-Pal/Pick_a_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_PalApp.swift
+//  Pick-a-Pal
+//
+//  Created by 森部高昌 on 2026/05/03.
+//
+
+import SwiftUI
+
+@main
+struct Pick_a_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
