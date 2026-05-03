2dfe77e3a0f549911a25ec02c46f22cddef93c05
森部高昌 <takamasamoribe@gmail.com>
2026-05-03 11:10:58 +0900

    2026-0503-03

--- Pick-a-Pal/ContentView.swift
+++ Pick-a-Pal/ContentView.swift
@@ -9,15 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var names: [String] = ["Elisha", "Andre", "Jasmine", "Po-Chun"]
+    @State private var nameToAdd = ""
     
     var body: some View {
         VStack {
             List {
                 ForEach(names, id: \.description) { name in
                     Text(name)
-                    
                 }
             }
+            
+            TextField("Add a name", text: $nameToAdd)
+                .background(Color.blue.opacity(0.5)) .cornerRadius(5)
+                .foregroundColor(.black)
+                .frame(width: 350, height: 100) // 幅200、高さ100に固定
+            
+                .autocorrectionDisabled(true)
+                .onSubmit {
+                    if !nameToAdd.isEmpty {
+                        names.append(nameToAdd)
+                        nameToAdd = ""
+                    }
+                    //nameToAdd = ""
+                }
         }
         .padding()
     }
